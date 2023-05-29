8d8c60f09e014bdcabe5b9c14a94cdffe526a2e1
KuczeQ <65510168+KuczeQ@users.noreply.github.com>
2023-05-29 17:37:24 +0200

    main

---
+++ bot.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import os
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=".", intents=discord.Intents.all())
+
+    async def setup_hook(self):
+        for filename in os.listdir("./komendyetc"):
+            if filename.endswith(".py"):
+                await self.load_extension(f"komendyetc.{filename[:-3]}")
+                await self.tree.sync()
+
+    async def reload_hook(self):
+        for filename in os.listdir('./komendyetc'):
+            if filename.endswith('.py'):
+                await self.reload_extension(f'komendyetc.{filename[:-3]}')
+                await self.tree.sync()
+
+    async def on_ready(self):
+        print(f"{self.user} połączył się z Discordem.")
+
+def get_token():
+    with open("token.txt", "r") as file:
+        token = file.read().strip()
+    return token
+
+bot = Bot()
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    print('Made by Bartuś O❤#4052 / https://github.com/KuczeQ/BoxBot')
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(name="github.com/KuczeQ", type=discord.ActivityType.watching))
+
+bot.run(get_token())
