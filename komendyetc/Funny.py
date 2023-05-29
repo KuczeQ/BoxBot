dd5345142bdfdeb5e7ec645dec864c65a7724978
KuczeQ <65510168+KuczeQ@users.noreply.github.com>
2023-05-29 17:47:12 +0200

    Update Funny.py

--- komendyetc/Funny.py
+++ komendyetc/Funny.py
@@ -8,7 +8,7 @@ class Funny(commands.Cog):
     def __init__(self, bot:commands.Bot) -> None:
         self.bot=bot
 
-    @app_commands.command(name="Random Number", description="A random number from the range selected by the user")
+    @app_commands.command(name="randomnumber", description="A random number from the range selected by the user")
     async def randomn(self, inte:discord.interactions, frome: int, to: int):
         embed=discord.Embed(title=f"Random Number: {random.randint(frome, to)}", color=0x00ff00)
         await inte.response.send_message(embed=embed)
